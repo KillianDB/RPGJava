07c5f42e98998d6ffe5c96b8b52f6d9fb62b1ca5
Killian D.B <115996000+KillianDB@users.noreply.github.com>
2023-10-11 20:44:03 -0300

    ajustando sintaxe da classe mago
    modificar a main de acordo com as novas mudanças

--- Mago.java
+++ Mago.java
@@ -1,12 +1,13 @@
+import java.util.Random;
+
 public class Mago {
-    public String nome;
-    public int resistencia;
-    public int forca;
-    public int inteligencia;
-    public int vida = 10 * resistencia;
-    public int magia = 10 * inteligencia;
-    public int n;
-    public int dano;
+    private String nome;
+    private int resistencia;
+    private int forca;
+    private int inteligencia;
+    private int vida = 10 * resistencia;
+    private int magia = 10 * inteligencia;
+    private Random n = new Random();
 
     public Mago(String nome, int resistencia, int forca, int inteligencia) {
         this.nome = nome;
@@ -34,81 +35,81 @@ public class Mago {
     }
 
     public String getNome() {
-        return nome;
+        return this.nome;
     }
 
     public int getResistencia() {
-        return resistencia;
+        return this.resistencia;
     }
 
     public int getForca() {
-        return forca;
+        return this.forca;
     }
 
     public int getInteligencia() {
-        return inteligencia;
+        return this.inteligencia;
     }
 
     public int getVida() {
-        return vida;
+        return this.vida;
     }
 
     public int getMagia() {
-        return magia;
+        return this.magia;
     }
 
-    public int atacar(int n) {
-        this.n = n;
-        return forca * n;
+    public int atacar() {
+        int ataque = this.forca * n.nextInt(10)+1;
+        return ataque;
     }
 
     public String moveParaFrente() {
-        return nome + " move para frente.";
+        return this.nome + " move para frente.";
     }
 
     public String receberDano(int dano) {
         String resposta;
-        if (vida > 0) {
-            this.vida = vida - dano;
-            resposta = nome + " está com " + vida + " pontos de vida.";
+        if (this.vida > 0) {
+            this.vida = this.vida - dano;
+            resposta = this.nome + " está com " + this.vida + " pontos de vida.";
         } else {
-            resposta = nome + " está morto.";
+            resposta = this.nome + " está morto.";
         }
 
         return resposta;
     }
 
-    public int bolaDeFogo(int n) {
-        this.magia = magia - 10;
-        return this.inteligencia * n;
+    public int bolaDeFogo() {
+        this.magia = this.magia - 10;
+        int danoBolaDeFogo = this.inteligencia * n.nextInt(10)+1;
+        return danoBolaDeFogo;
     }
 
     public String energiaRefletora(int dano) {
-        this.vida = vida + ((dano / 3) * 2);
-        this.n = dano;
-        return nome+ " usou Energia Refletora, recuperando 66% da vida perdida nessa rodada e concedendo a si o mesmo o dano aplicado nessa rodada.";
+        this.vida = this.vida + ((dano / 3) * 2);
+        return this.nome+ " usou Energia Refletora, recuperando 66% da vida perdida nessa rodada e concedendo a si o mesmo o dano aplicado nessa rodada.";
     }
 
-    public String transformacao(int n) {
+    public String transformacao() {
         setForca(this.forca + n);
         setResistencia(this.resistencia + n);
-        return nome + " usou transformação, adiquirindo n pontos a mais de força e resistencia.";
+        return this.nome + " usou transformação, adiquirindo n pontos a mais de força e resistencia.";
     }
 
     public String congelamento() {
         return this.nome+" foi congelado por uma rodada e não poderá jogar.";
     }
 
+    //criar outra classe para criar outro personagem de ajuda, lacaio por exemplo
     public String invocacaoElemental() {
        Mago novo = new Mago("Rodolfo", 4, 3, 3);
-        return nome + " invocou um mago elemental para auxilia-lo na batalha. Nome: "+novo.nome+", força: "+novo.forca+", resistencia: "+novo.resistencia+", inteligencia: "+novo.inteligencia;
+        return this.nome + " invocou um mago elemental para auxilia-lo na batalha. Nome: "+this.nome+", força: "+this.forca+", resistencia: "+this.resistencia+", inteligencia: "+this.inteligencia;
     }
 
-    public String maldicao(Mago alvo, int n) {
+    public String maldicao(Mago alvo) {
         int duracao=0;
-        this.n=n;
         for (int rodada = 1; rodada <= duracao; rodada++) {
-            alvo.receberDano(n);
+            alvo.receberDano(n.nextInt(10)+1);
             duracao++;
         }
         return alvo + " foi amaldiçoado e receberá " + n + " de dano no começo de cada nova rodada.";
